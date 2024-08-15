5e87e730bfcc165f1b5d38af1fca8e36b7253718
Ernest Spiller <e.spiller2002@gmail.com>
2024-08-15 14:47:14 -0700

    Updated main.cpp
    - Initialized the variables for the I/O pins

--- src/main.cpp
+++ src/main.cpp
@@ -10,35 +10,73 @@ that correspond to the proper morse code.
 
 After holding the button for 'x' amount of time, it will clear the display.
 
-The circuit:
+The circuit (LCD module):
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
  * LCD D4 pin to digital pin 5
  * LCD D5 pin to digital pin 4
  * LCD D6 pin to digital pin 3
  * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
  * LCD VSS pin to ground
  * LCD VCC pin to 5V
  * 10K resistor:
    * ends to +5V and ground
    * wiper to LCD VO pin (pin 3)
 
+The circuit (Button & RGB light):
+ * SW1 push button pin to digital pin 22
+ * D1 pin 2 to digital pin 28
+ * D1 pin 3 to digital pin 25
+ * D1 pin 4 to digital pin 24
 */
 
-// put function declarations here:
-int myFunction(int, int);
+// Defining the variables for the digital pin I/O on LCD
+const int rs = 12, en = 11;
+const int d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Defining the variables for the digital pin I/O on RGB & Button
+const int button = 22, r = 28, g = 25, b = 24;
+
+// Defining variable for button presses & time calculations
+bool pressed; // boolean value for whether or not the button is pressed
+unsigned long lastPressTime = 0; // to track the time of the last button press
+const unsigned long debounceDelay = 50; // debounce time in milliseconds
+
+// Function containing 
+void morse_alphabet() {
+
+}
+
+// Defining the list for holding the set of input from button
+int inputArray[4]; // defines a fixed-sized array of 4
+
+// Initializes the LCD display settings
+void LCD_settings() {
+  lcd.leftToRight(); // go left for the next letters
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initializes the I/O pins for input or output
+  int pin; // initializing the pin variable for the board
+  for (pin = d4; pin <= d7; pin++)
+  {
+    pinMode(pin, OUTPUT);
+  }
+
+  // Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  // Turn off the display:
+  lcd.noDisplay();
+  delay(500);
+  // Turn on the display:
+  lcd.display();
+  delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  unsigned current_time = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  lcd.setCursor(0, 1); // sets cursor to column 0, line 1
 }
