b36daafbb2bb8d8b9287ce7b8a1dd9973993439e
Ernest Spiller <ernestspiller@proton.me>
2024-08-16 21:50:07 -0700

    Button press detection in main.cpp
    - Added feature to detect button press duration as well as time between each press
    - Added function 'set_color' to change the color of the RGB LED

--- src/main.cpp
+++ src/main.cpp
@@ -46,22 +46,9 @@ unsigned long lastPressTime = 0; // to track the time of the last button press
 const unsigned long debounceDelay = 50; // debounce time in milliseconds
 unsigned long pressStartTime = 0; // stores the time when the button was pressed
 unsigned long pressDuration = 0; // stroes the duration of the button press
+unsigned long pressNotDuration = 0; // stores duration of no press on button
 char shortPress = '0'; // defines the short press as char 0 
 char longPress = '1'; // defines the long press as char 1
-
-// Function to setup the digital pins for I/O
-void pin_setup() {
-  // Initializes the digital board pins for I/O
-  pinMode(button, INPUT); // sets button to read input
-  pinMode(r, OUTPUT); // sets RGB light pins to output
-  pinMode(g, OUTPUT);
-  pinMode(b, OUTPUT);
-  int pin; // initializing the pin variable for the board
-  for (pin = d4; pin <= d7; pin++)
-  {
-    pinMode(pin, OUTPUT);
-  }
-}
 
 /*
 Using an array of values 0 and 1 to define short and long presses
@@ -79,6 +66,26 @@ int morseCode[] = { // defining the array containing all morse code letters
 // Setting up the array to collect user input
 std::vector<char> morseCodeInput;
 
+// Function to setup the digital pins for I/O
+void pin_setup() {
+  // Initializes the digital board pins for I/O
+  pinMode(button, INPUT); // sets button to read input
+  pinMode(r, OUTPUT); // sets RGB light pins to output
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
+  int pin; // initializing the pin variable for the board
+  for (pin = d4; pin <= d7; pin++)
+  {
+    pinMode(pin, OUTPUT);
+  }
+}
+
+void set_color(int R, int G, int B) { // sets the color of the RGB light
+  analogWrite(r, R);
+  analogWrite(b, B);
+  analogWrite(g, G);
+}
+
 void setup() { // runs only once when the board turns on
   pin_setup(); // runs function to setup pins
   
@@ -94,16 +101,36 @@ void setup() { // runs only once when the board turns on
 }
 
 void loop() { // runs the whole time when the board is on
-  unsigned currentTime = millis(); // gets the current time since this block of code ran
 
-  if (digitalRead(button) == HIGH) // checks if the button is pressed
-  {
-    if (!isPressed && (currentTime - lastPressTime > debounceDelay)) // checks to see if it wasn't already pressed and debounce time passed
-    {
-      isPressed = true; // mark button as pressed in a variable
-      if ()
+  /*
+  Line 103-111 checks to see if the button is pressed or not and
+  calculates the duration of the press for morse code
+  */
+
+  if (digitalRead(button) == HIGH) { // if the button is pressed
+    if (!isPressed) { // ensures this is the first time the button is pressed
+      isPressed = true;
+      pressNotDuration = millis() - lastPressTime; // calculates the duration when button is not pressed
+      pressStartTime = millis(); // record the time when the button was pressed
+    }
+  } else { // if the button was not pressed
+    if (isPressed) { // if the button was previously pressed
+      isPressed = false;
+      lastPressTime = millis(); // records time when button is released
+      pressDuration = millis() - pressStartTime; // calculate the press duration
     }
   }
 
-  lcd.setCursor(0, 1); // sets cursor to column 0, line 1
+  /*
+  */
+
+  if (pressDuration <= 500) {
+    if (pressDuration <= 250) {
+      
+    }
+  } else {
+
+  }
+
+  lcd.setCursor(0, 1); // resets cursor to column 0, line 1
 }
